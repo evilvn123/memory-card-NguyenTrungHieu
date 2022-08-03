2d431ede2d902dc300296fb5c23046c6d0bb213f
evilvn123 <taducminh2004@gmail.com>
2022-08-03 11:17:11 +0700

    implement enzyme

--- src/__tests__/Board.test.tsx
+++ src/__tests__/Board.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Board from "../components/Board";
 import cards from "../data/cards";
+import { mount, shallow } from "enzyme";
+import Card from "../components/Card";
 
+import * as enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+enzyme.configure({ adapter: new Adapter() });
 it("Render Board", () => {
   render(<Board cards={cards} />);
   const cardComponent = screen.getAllByTestId("card");
   expect(cardComponent.length).toEqual(cards.length);
+});
+
+it("Render Card components in Board", () => {
+  const wrapper = shallow(<Board cards={cards} />);
+  expect(wrapper.find(Card)).toHaveLength(cards.length);
+  const sampleCard = wrapper.find(Card).at(0);
+  sampleCard.simulate("click");
 });
